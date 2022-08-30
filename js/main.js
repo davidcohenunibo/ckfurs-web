5c657f49300d53f4f5f5e3c934179133a80681c5
David <david.cohen@studio.unibo.it>
2022-08-30 10:32:22 +0200

    begin repo

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+	  setTimeout(function() {
+		$('.toggle-button').fadeOut('fast');
+	}, 100); // <-- time in milliseconds
+  });
+
+})(jQuery);
